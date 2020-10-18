b5f84df5521cc414ae3e524c525a5249b23958b9
pranvi1302 <pranvikant.mishra@gmail.com>
2020-10-18 17:27:03 +0530

    ok

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function preload(){
+trackImage=loadImage("track1.jpg")
+carImage=loadImage("car2.png")
+}
+function setup(){
+createCanvas(1200,1000)
+}
+function draw(){
+image(trackImage,600,600)
+}
